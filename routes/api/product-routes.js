d058a755d5dee5ce5b3bb7b0106e254c40fc05e4
Danny Roubin <dannyroubin1@gmail.com>
2021-07-26 14:48:04 -0700

    minor bug fixes to the product route, app is completely working now

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag }],
+      include: [{ model: Category }],
     });
     if (!productData) {
       res.status(404).json({ message: "No products matching the field found" });
@@ -26,17 +26,11 @@ router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({
+    const productData = await Product.findByPk(req.params.id, {
       include: [
         {
           model: Category,
           attributes: ["id", "category_name"],
-        },
-        {
-          model: Tag,
-          attributes: ["id", "tag_name"],
-          through: ProductTag,
-          as: "tags",
         },
       ],
     });
@@ -60,12 +54,7 @@ router.post("/", async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create({
-    product_name: req.body.product_name,
-    price: req.body.price,
-    stock: req.body.stock,
-    tagIds: req.body.tagIds,
-  })
+  Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -90,19 +79,11 @@ router.post("/", async (req, res) => {
 // update product
 router.put("/:id", async (req, res) => {
   // update product data
-  Product.update(
-    {
-      product_name: req.body.product_name,
-      price: req.body.price,
-      stock: req.body.stock,
-      tagIds: req.body.tagIds,
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
     },
-    {
-      where: {
-        id: req.params.id,
-      },
-    }
-  )
+  })
     .then(() => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
